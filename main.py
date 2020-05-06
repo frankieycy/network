7df16381c9615a8659e217c994f922f8a3c04925
frankieycy <frankieycy@gmail.com>
2020-05-05 19:52:42 -0700

    cov ratio matrix

--- main.py
+++ main.py
@@ -2,23 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import network as n
 
-def f(x):
-    return 0
-
-def h(xi,xj):
-    return xi-xj
-
 # g = n.graph(15,.2,1)
 # g.printAdjacency('adj.txt')
 # g.printCoupling('cou.txt')
 
-# g = n.graph(15,adjacencyFile='adj.txt',couplingFile='cou.txt')
+# g = n.graph(adjacencyFile='adj.txt')
 # print(g.getLaplacian())
 
 s = 100
-g = n.network(s,intrinsicFunc=f,couplingFunc=h,noiseSigma=.1,coupling=1,connectProb=.05)
+g = n.network(size=s,connectProb=.2,noiseSigma=1)
 g.printAdjacency('adj.txt')
-g.initDynamics(np.random.rand(s))
-g.runDynamics(.01,10)
-g.printDynamics('data.csv')
-g.plotDynamics('data.png')
+g.initDynamics(np.random.uniform(0,1,s))
+g.runDynamics(5e-4,50)
+g.estimateConnectivity()
--- network.py
+++ network.py
@@ -3,23 +3,29 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('text', usetex=True)
 
+progressBars = ['-','\\','|','/']
+
 class graph:
-    def __init__(self, size, coupling=1, connectProb=0, adjacencyFile=None):
+    def __init__(self, adjacencyFile=None, size=0, coupling=1, connectProb=0):
         # uniform bidirectional coupling
-        self.size = size
+        # (i) load adjacency matrix (require size, coupling, connectProb), or
+        # (ii) create adjacency matrix (require coupling, adjacencyFile)
         self.coupling = coupling
 
+        # adjacency matrix
         if adjacencyFile: # load adjacency matrix from file
+            print('loading adjacency file from %s...'%adjacencyFile)
             self.loadAdjacency(adjacencyFile)
-            assert(self.Adjacency.shape[0]==self.Adjacency.shape[1]==self.size)
-        else: # construct random adjacency matrix
+            self.size = self.Adjacency.shape[0]
+        else: # create random adjacency matrix
+            self.size = size
             self.Adjacency = np.zeros((self.size,self.size),dtype=int)
-            self.Coupling = np.zeros((self.size,self.size))
             for i in range(self.size):
                 for j in range(i+1,self.size):
                     if np.random.uniform()<connectProb:
                         self.Adjacency[i][j] = self.Adjacency[j][i] = 1
 
+        # coupling matrix
         self.Coupling = self.coupling*self.Adjacency
 
         # node degrees
@@ -46,11 +52,11 @@ class graph:
         return self.degrees
 
     def getLaplacian(self):
-        return self.Laplacian
+        return self.laplacian
 
     def printAdjacency(self, file=None):
         if file:
-            f = open(file, 'w')
+            f = open(file,'w')
             f.write('# adjacency matrix of size %d\n'%self.size)
             for row in self.Adjacency:
                 for val in row[:-1]:
@@ -58,42 +64,43 @@ class graph:
                 f.write('%d\n'%row[-1])
             f.close()
         else:
-            print(self.Adjacency)
-
-    # def printCoupling(self, file=None):
-    #     if file:
-    #         f = open(file, 'w')
-    #         f.write('# coupling matrix of size %d\n'%self.size)
-    #         for row in self.Coupling:
-    #             for val in row[:-1]:
-    #                 f.write('%d '%val)
-    #             f.write('%d\n'%row[-1])
-    #         f.close()
-    #     else:
-    #         print(self.Coupling)
+            print(m)
 
     def loadAdjacency(self, file):
         self.Adjacency = np.loadtxt(file,dtype=int,delimiter=' ',comments='#')
-
-    # def loadCoupling(self, file):
-    #     self.Coupling = np.loadtxt(file,delimiter=' ',comments='#')
+        assert self.Adjacency.shape[0]==self.Adjacency.shape[1], 'adjacency matrix from %s not a square matrix'%file
 
 class network(graph):
-    # TO DO: generalize to generic dynamics, arbitrary f & h
-    def __init__(self, size, intrinsicFunc, couplingFunc, noiseSigma,\
-        coupling=1, connectProb=0, adjacencyFile=None):
-        super().__init__(size, coupling, connectProb, adjacencyFile)
-        self.intrinsicFunc = intrinsicFunc
-        self.couplingFunc = couplingFunc
-        self.noiseSigma = noiseSigma
+    def __init__(self, dynamicsFile=None, adjacencyFile=None, size=0, coupling=1, connectProb=0, noiseSigma=0):
+        # (i) load dynamics (require dynamicsFile)
+        # (ii) generate dynamics without adjacencyFile (require size, coupling, connectProb, noiseSigma)
+        # (iii) generate dynamics with adjacencyFile (require adjacencyFile, coupling, noiseSigma)
+        self.avgStates_ = []
+        self.Covariance = None
+        self.CovarianceInv = None
+        self.CovarianceRatios = None
 
-        self.initStates = None # initial states
-        self.states = None # current states
-        self.states_ = {i:[] for i in range(self.size)} # time series of states
-        self.time = 0
-        self.time_ = [0]
-        self.timeStep = None
-        self.iter = 1
+        if dynamicsFile: # load dynamics from file
+            print('loading dynamics file from %s...'%dynamicsFile)
+            data = np.loadtxt(dynamicsFile,delimiter=',',skiprows=1)
+            self.size = data.shape[1]-1
+            self.time_ = data[:,0].tolist()
+            self.time = self.time_[-1]
+            self.timeStep = self.time_[1]-self.time_[0]
+            self.iter = len(self.time_)
+            self.states_ = {i:data[:,i+1].tolist() for i in range(self.size)}
+
+        else: # generate dynamics
+            super().__init__(adjacencyFile, size, coupling, connectProb)
+            self.noiseSigma = noiseSigma
+
+            self.initStates = None # initial states
+            self.states = None # current states
+            self.states_ = {i:[] for i in range(self.size)} # time series of states
+            self.time = 0
+            self.time_ = [0]
+            self.timeStep = None
+            self.iter = 1
 
     def getStates(self):
         return self.states
@@ -101,19 +108,65 @@ class network(graph):
     def getDynamics(self):
         return self.states_
 
+    def setCovariance(self):
+        print('computing covariance matrix...')
+        self.setAvgStates()
+
+        self.Covariance = np.zeros((self.size,self.size))
+        counter = 0
+        # IMPROVE: speed up the computation!
+        for i in range(self.size):
+            for j in range(i,self.size):
+                cov = 0
+                for t in range(self.iter):
+                    cov += (self.states_[i][t]-self.avgStates_[t])*(self.states_[j][t]-self.avgStates_[t])
+                cov /= self.iter
+                self.Covariance[i][j] = self.Covariance[j][i] = cov
+
+                counter += 1
+                print('%c %.2f %% complete\r'%(progressBars[(self.size*i+j)%4],100.*counter/(self.size*(self.size+1)/2)),end='')
+
+    def setAvgStates(self):
+        for t in range(self.iter):
+            avg = 0
+            for i in range(self.size):
+                avg += self.states_[i][t]
+            avg /= self.size
+            self.avgStates_.append(avg)
+
+    def estimateConnectivity(self):
+        self.setCovariance()
+
+        print('estimating connectivity...')
+        self.CovarianceInv = np.linalg.pinv(self.Covariance)
+
+        self.CovarianceRatios = np.zeros((self.size,self.size))
+        for i in range(self.size):
+            for j in range(i+1,self.size):
+                self.CovarianceRatios[i][j] = self.CovarianceRatios[j][i] = self.CovarianceInv[i][j]/self.CovarianceInv[i][i]
+
+        # print(self.Covariance[0])
+        # print(self.CovarianceInv[0])
+        print(self.CovarianceRatios[0])
+
+        plt.scatter(list(range(self.size)),sorted(self.CovarianceRatios[0]))
+        plt.show()
+
     def printDynamics(self, file):
         # print node states as time series to file
+        print('printing dynamics to %s...'%file)
         f = open(file, 'w')
         f.write('time,')
         for i in range(self.size-1): f.write('%d,'%i)
         f.write('%d\n'%(self.size-1))
-        for i in range(self.iter):
-            f.write('%.4f,'%(i*self.timeStep))
-            for j in range(self.size-1): f.write('%.4f,'%self.states_[j][i])
-            f.write('%.4f\n'%self.states_[self.size-1][i])
+        for t in range(self.iter):
+            f.write('%.4f,'%(t*self.timeStep))
+            for i in range(self.size-1): f.write('%.4f,'%self.states_[i][t])
+            f.write('%.4f\n'%self.states_[self.size-1][t])
         f.close()
 
     def plotDynamics(self, file):
+        print('plotting dynamics to %s...'%file)
         fig = plt.figure(figsize=(12,6))
         plt.xlim(0,self.time)
         for i in range(self.size): plt.plot(self.time_,self.states_[i])
@@ -132,11 +185,11 @@ class network(graph):
 
     def getRates(self):
         # instantaneous node rates
-        # CONSENSUS DYNAMICS, f & h implicit
+        # CONSENSUS DYNAMICS: dx_i/dt = -g sum_j L_ij * x_j + eta_j
         return -self.coupling*self.laplacian.dot(self.states)+\
             np.random.normal(0,self.noiseSigma,self.size)
 
-    def runDynamics(self, timeStep, endTime):
+    def runDynamics(self, timeStep, endTime, silent=True):
         # evolve dynamics according to dynamical equations
         self.timeStep = timeStep
         while self.time<endTime:
@@ -147,3 +200,11 @@ class network(graph):
             self.time += self.timeStep
             self.time_.append(self.time)
             self.iter += 1
+
+            if silent:
+                print('t = %6.2f | %c %.2f %%\r'%(self.time,progressBars[self.iter%4],100*self.time/endTime),end='')
+            elif self.iter%100==0:
+                print('t = %6.2f | '%self.time,end='')
+                for i in range(min(self.size,4)):
+                    print('x%d = %6.2f | '%(i,self.states[i]),end='')
+                if self.size>4: print('...')
